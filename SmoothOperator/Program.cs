1c02c9477aaca1a0197705e8c9a77d285fbe3ea2
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-02-28 17:13:38 +0000

    Ask user a non-negative number and print n-- and n++

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //ask user for number
+            Console.Write("Insere número inteiro não-negativo: ");
+            
+
+            //get number from user and convert it to byte
+            string s = Console.ReadLine();
+            byte n = byte.Parse(s);
+
+            //increment and decrement number received
+
+            byte decrement = n--;
+            byte increment = n++;
+
+            Console.WriteLine(increment);
+            Console.WriteLine(decrement);
+
+            
+
+
+
+
+       
+            
+            
+        }
+    }
+}
