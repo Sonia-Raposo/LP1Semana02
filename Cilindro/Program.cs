3efeb887d42a4a157a4e0fce55f95337bc77f130
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-02-28 18:18:06 +0000

    Added comments

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -6,27 +6,32 @@ namespace Cilindro
     {
         static void Main(string[] args)
         {
+            //ask height and save it as a float
             Console.Write("Insira a altura do cilindro em cm: ");
 
             string altura = Console.ReadLine();
             float a = float.Parse(altura);
 
+
+            //ask radius and save it as a float
             Console.Write("Insira o raio do cilindro em cm: ");
 
             string raio = Console.ReadLine();
             float r = float.Parse(raio);
 
-            
+
+            //calculate cilinder volume
             float v = MathF.PI * MathF.Pow(r, 2) * a;
 
+            //calculate cilinder area
             float s = 2*MathF.PI * r*(r+ a);
 
 
+            //print the result of volume and area
             Console.WriteLine("Volume do Cilindro em cm3: ");
             Console.WriteLine(v);
             Console.WriteLine("Área do Cilindro em cm2: ");
             Console.WriteLine(s);
-
 
             
         }
