3533929404eb666366e4a370d6e588d8e0ce61bf
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-02-28 18:03:22 +0000

    Add ask the user for height and radius of the cilinder

---
+++ Cilindro/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira a altura do cilindro: ");
+
+            string altura = Console.ReadLine();
+            float a = float.Parse(altura);
+
+            Console.Write("Insira o raio do cilindro: ");
+
+            string raio = Console.ReadLine();
+            float r = float.Parse(raio);
+
+
+            
+        }
+    }
+}
