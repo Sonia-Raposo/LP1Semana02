4ec5d6b4cb1934da8dcab83bad85e3183972a9dd
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-03-01 13:47:01 +0000

    Added exit condition and print 100 'white lines'

---
+++ MasterAndPlayer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+
+namespace ChatGPTOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string question;
+
+
+            do
+            {
+
+                Console.Write("Number Master insert number: ");
+                string answer  = Console.ReadLine();
+
+                int n = int.Parse(answer);
+
+
+                if (n < 0 || n > 100)
+                {
+                    Console.WriteLine("Invalid number!");
+                    break;
+                }
+                
+                else
+                {
+                    string line = "";
+
+                    for (int x = 0 ; x == 100; x++)
+                    {
+                        Console.WriteLine(line);
+                    }
+
+                    
+                }
+
+
+            } while (true);                    
+                
+        }
+           
+    }
+}
